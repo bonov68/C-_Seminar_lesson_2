9414662924b670cf530b7fdf9edfe97ff5c9e4f9
bonov68 <example@mail.ru>
2022-12-01 20:27:55 +0300

    theory finish

--- Explanation_lesson2/Program.cs
+++ Explanation_lesson2/Program.cs
@@ -4,10 +4,48 @@
 // Console.WriteLine(result);
 
 // нах макс, мин числа
-int a= -4;
-int b = 3; 
-// int result = Math.Max(a, b);
-int result = Math.Min(a, b);
-Console.WriteLine(result);
+// int a= -4;
+// int b = 3; 
+// // int result = Math.Max(a, b);
+// int result = Math.Min(a, b);
+// Console.WriteLine(result);
 
+// возведение в степень
+// int a = 2;
+// int b = 4; 
+// double result = Math.Pow(a, b);
+// Console.WriteLine(result);
 
+// округление чисел
+// double a = 2.45663;
+// int b = 4; 
+// double result = Math.Round(a, 2);
+// Console.WriteLine(result);
+
+// условные циклы
+// int a = 7;
+// while (a > 0)
+// {
+//     Console.WriteLine(a);
+//     a--;
+// }
+// Console.WriteLine("все");
+
+// continue
+//  int a = 7;
+// while (a > 0)
+// {
+// a--;
+// if ((a ==6) || (a ==4))
+// {
+// continue;
+// }
+// Console.WriteLine(a);
+// }
+
+// цикл for
+// for (int i = 1; i < 5; i++)
+// {
+// Console.WriteLine(i);
+// }
+// Console.WriteLine("все");
